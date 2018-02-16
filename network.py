967b16738914076969abe88a236c6645b187676a
Michael Williams <michael.williams@zxilix.com>
2018-02-16 12:27:21 +0000

    Added code to load datasets

--- network.py
+++ network.py
@@ -15,6 +15,7 @@ from keras.layers import Input, Conv1D, MaxPool1D, Dense
 from keras.models import Sequential
 from keras.optimizers import Nadam
 from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 
 np.random.seed(1337)
 
@@ -68,17 +69,36 @@ def optimizer():
 
 def main(args):
 
-    # load data
-    #TODO: load data
-    x_train = 0
-    y_train = 0
+    # load an image to use as a reference
+    img = load_img('data/raw_data30/chopin/chopin0.png')
+    x = img_to_array(img)
+    x = x.reshape((1,) + x.shape)
+    # get shape of image
+    img_shape = x.shape[1:3]
 
-    x_test = 0
-    y_test = 0
+    # augemntation configuration
+    datagen = ImageDataGenerator(rescale=1. / 255)
 
+    # generator to read in images from directories
+    train_generator = datagen.flow_from_directory(
+        './data/data30/train',
+        target_size=(img_shape),
+        batch_size=args.batch_size,
+        class_mode='categorical'
+    )
+
+    test_generator = datagen.flow_from_directory(
+        './data/data30/test',
+        target_size=(img_shape),
+        batch_size=args.batch_size,
+        class_mode='categorical'
+    )
+
+    # input shape
+    input_shape = (None,) + x.shape[1:]
 
     # build network
-    model = build_model()
+    model = build_model(args, input_shape=input_shape)
 
     # compile network
     model.compile(
@@ -90,24 +110,29 @@ def main(args):
     model.summary()
 
     # callbacks for network
-    modelCheck = ModelCheckpoint('./best_weights.hdf5', monitor='loss', save_best_only=True, save_weights_only=True)
+    modelCheck = ModelCheckpoint('./best_weights.h5', monitor='loss', save_best_only=True, save_weights_only=True)
 
     # fit model
-    history = model.fit(x_train, y_train,
-                        epochs=args.epochs,
-                        batch_size=args.batch_size,
-                        callbacks=[modelCheck])
+    history = model.fit_generator(
+        train_generator,
+        steps_per_epoch=len(train_generator.filenames) // args.batch_size,
+        epochs=args.epochs,
+        callbacks=[modelCheck])
 
     # load best model
-    model.load_weights('./best_weights.hdf5}')
+    model.load_weights('./best_weights.h5')
 
     # evaluate results
-    eval_results = model.evaluate(x_test, y_test,
-                                  batch_size=args.batch_size,
-                                  verbose=1)
+    eval_results = model.evaluate_generator(
+        test_generator,
+        steps=(len(test_generator.filenames) // args.batch_size)
+    )
 
     # predictions from test set
-    preds = model.predict(x_test)
+    preds = model.predict_generator(
+        test_generator,
+        steps=(len(test_generator.filenames) // args.batch_size)
+    )
 
 if __name__ == '__main__':
     args = args()
